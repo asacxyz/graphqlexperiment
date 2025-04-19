a63c4d27d834ff031c43fd1eefda00df420bda00
asacxyz <andrealck1@gmail.com>
2025-04-19 16:20:30 +0000

    chore: init

---
+++ src/main/java/com/asacxyz/graphqlexperiment/GraphqlexperimentApplication.java
@@ -0,0 +1,13 @@
+package com.asacxyz.graphqlexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlexperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlexperimentApplication.class, args);
+	}
+
+}
